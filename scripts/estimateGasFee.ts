7d3483399fd7a865e9f0e8e92145ea1605359260
Lapo4kaKek <Sheydos2003@yandex.ru>
2025-05-09 00:00:41 +0300

    feat: added command for estimateGasFee and cut dynamic contractName

--- scripts/estimateGasFee.ts
+++ scripts/estimateGasFee.ts
@@ -1,20 +1,20 @@
 /// npx hardhat run --network sepolia scripts/estimateGasFee.ts
 import "dotenv/config";
 import { ethers } from "hardhat";
-import { JsonRpcProvider, Wallet, formatEther, formatUnits  } from "ethers";
+import { JsonRpcProvider, Wallet, formatEther, formatUnits } from "ethers";
 
 async function estimateDeployment(contractName: string) {
   if (!process.env.PRIVATE_KEY || !process.env.SEPOLIA_RPC_URL) {
     throw new Error("Missing .env variables!");
   }
   const provider = new JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
-  const wallet   = new Wallet(process.env.PRIVATE_KEY, provider);
+  const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
 
   const Token1 = await ethers.getContractFactory(contractName, wallet);
-  
+
   const deploymentData = Token1.interface.encodeDeploy([
     process.env.USDT_ADDRESS,
-    process.env.TREASURY_ADDRESS
+    process.env.TREASURY_ADDRESS,
   ]);
 
   const bytecode = Token1.bytecode;
@@ -24,28 +24,31 @@ async function estimateDeployment(contractName: string) {
 
   const estimatedGasLimit = await provider.estimateGas({
     from: await wallet.getAddress(),
-    data: fullDeploymentData
+    data: fullDeploymentData,
   });
 
   let gasCost;
   if (feeData.maxFeePerGas) {
     gasCost = estimatedGasLimit * feeData.maxFeePerGas;
-    console.log(`Max price for gas: ${formatUnits(feeData.maxFeePerGas, 'gwei')} Gwei`);
+    console.log(
+      `Max price for gas: ${formatUnits(feeData.maxFeePerGas, "gwei")} Gwei`
+    );
   } else if (feeData.gasPrice) {
     gasCost = estimatedGasLimit * feeData.gasPrice;
-    console.log(`Gas price: ${formatUnits(feeData.gasPrice, 'gwei')} Gwei`);
+    console.log(`Gas price: ${formatUnits(feeData.gasPrice, "gwei")} Gwei`);
   } else {
     throw new Error("Gas price data could not be obtained");
   }
 
   console.log("\n--- Gas assessment results ---");
-  console.log(`Assessment of the gas limit: ${estimatedGasLimit.toString()} unit`);
-  
+  console.log(
+    `Assessment of the gas limit: ${estimatedGasLimit.toString()} unit`
+  );
+
   console.log("\n--- Price for deploy ---");
   console.log(`Wei: ${gasCost.toString()} Wei`);
-  console.log(`Gwei: ${formatUnits(gasCost, 'gwei')} Gwei`);
+  console.log(`Gwei: ${formatUnits(gasCost, "gwei")} Gwei`);
   console.log(`ETH: ${formatEther(gasCost)} ETH`);
-  
 }
 
 estimateDeployment("Token1").catch(console.error);
